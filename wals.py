726b24531da9483773960f8e9d5f119c5c85851d
Scott Mackie <mr.jsmackie@gmail.com>
2024-03-10 21:54:10 -0700

    change response structure

--- wals.py
+++ wals.py
@@ -3,39 +3,41 @@ from langchain_openai import ChatOpenAI as LangChainChatOpenAI
 from langchain.chains import RetrievalQA
 from HidingBehindWALS import setup_vectorstore
 
+
 class WALSBuilder:
 
-  def __init__(self, open_ai_key):
-    self.agent = self.setup_agent(open_ai_key)
-    self.preamble = """
+    def __init__(self, open_ai_key):
+        self.agent = self.setup_agent(open_ai_key)
+        self.preamble = """
     You are an expert on the World Atlas of Language Structures, also called WALS.
     Assume that any questions you are asked are referring to WALS, so if someone says Chapter 12
     they mean Chapter 12 in WALS, if they ask about verbal morphology they really mean what does
     WALS say about verbal morphology. Act like a helpful librarian who knows about WALS.
     """
 
-  def TalkToWALS(self, query):
-    response = self.agent.invoke('\n'.join([self.preamble, query]))
-    print(response['result'])
+    def TalkToWALS(self, query):
+        response = self.agent.invoke('\n'.join([self.preamble, query]))
+        print(response['result'])
 
-  def setup_agent(self, api_key):
-    index, embeddings = setup_vectorstore(make_new_index=False,
-                                        upsert_metadata=False,
-                                        upsert_chapter_data=False)
-    vectorstore = LangChainPinecone(index, embeddings, text_key='text')
+    def setup_agent(self, api_key):
+        index, embeddings = setup_vectorstore(make_new_index=False,
+                                              upsert_metadata=False,
+                                              upsert_chapter_data=False)
+        vectorstore = LangChainPinecone(index, embeddings, text_key='text')
 
-    llm = LangChainChatOpenAI(
-        openai_api_key=api_key,
-        model_name='gpt-3.5-turbo',
-        temperature=0.9
-    )
+        llm = LangChainChatOpenAI(
+            openai_api_key=api_key,
+            model_name='gpt-3.5-turbo',
+            temperature=0.9
+        )
 
-    agent = RetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=vectorstore.as_retriever()
-    )
-    return agent
+        agent = RetrievalQA.from_chain_type(
+            llm=llm,
+            retriever=vectorstore.as_retriever()
+        )
+        return agent
 
-  def interact(self, message, history):
-    response = self.agent.invoke('\n'.join([self.preamble, message]))
-    return response
+    def interact(self, message, history):
+
+        response = self.agent.invoke(''.join([self.preamble, message]))
+        return response['result']
